403d93d22a7fb01d063e0e72677fe6152075af6f
呵呵 <xx312472644@163.com>
2026-04-29 16:33:08 +0800

    docs(build-doc-deps): 更新文档构建脚本，添加依赖安装逻辑
    1. 添加依赖模块检查逻辑
    2. 在构建前自动安装指定依赖
    3. 优化控制台输出格式

--- docs/build-doc-deps.js
+++ docs/build-doc-deps.js
@@ -1,6 +1,9 @@
 const { execSync } = require("child_process");
 const pkg = require("./package.json");
 
+// 是否需要重新安装依赖
+const dependenciesModules = ["@develop-plugins/excel-preview"];
+
 function getDeps() {
   const deps = [];
   for (const dep in pkg.dependencies) {
@@ -15,8 +18,13 @@ function getDeps() {
 function executeBuild() {
   const deps = getDeps();
   for (const depName of deps) {
+    if (dependenciesModules.includes(depName)) {
+      const installCmd = `pnpm --filter ${depName} i`;
+      console.log(`⚒️ 安装依赖: ${depName} 【${installCmd}】⚒️`);
+      execSync(installCmd, { stdio: "inherit" });
+    }
     const buildCmd = `pnpm --filter ${depName} run build`;
-    console.log(`🛠 当前构建命令: 【${buildCmd}】`);
+    console.log(`⚒️ 当前构建命令: 【${buildCmd}】⚒️`);
     execSync(buildCmd, { stdio: "inherit" });
   }
 }
